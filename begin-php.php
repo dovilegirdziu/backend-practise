8bb694a19ff257a2b1276b659a46d3f15735e97d
dovilegirdziu <97856821+dovilegirdziu@users.noreply.github.com>
2022-11-27 19:00:56 +0200

    get footer, get header

--- begin-php.php
+++ begin-php.php
@@ -56,3 +56,54 @@
 
 </body>
 </html>
+
+
+<!-- Seperating footer and header -->
+<?php get_header(); ?>
+
+  <div id="content">
+
+  <!-- Add any template tags outside of loop -->
+
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <!-- Add any post template tags inside of loop -->
+
+      <article <?php post_class(); ?> >
+
+      
+      <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+
+      </article>
+
+      <section>
+        <p class="byline">
+          Author:
+          <a href="<?php echo get_author_posts_url( $post->post_author );  ?>">
+          <?php the_author(); ?></a> 
+        </p>
+        <p>
+          Date:
+          <?php the_time( 'M. j, Y' ); ?> |
+          Categories: 
+          <?php the_category( ',' ) ?> |
+          Tags:
+          <?php the_tags( '',',','' ); ?>
+        </p>
+      </section>
+
+      <?php endwhile; else: ?>
+
+        <h2><?php esc_html_e( '404 Error', 'phpforwp' ); ?></h2>
+        <p><?php esc_html_e( 'Sorry, content not found.', 'phpforwp' ); ?></p>
+
+      <?php endif; ?>
+
+  <!-- Add any template tags outside of loop -->
+
+    </div>
+
+    <?php get_footer(); ?>
+
+
